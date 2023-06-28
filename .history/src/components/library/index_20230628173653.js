f92ddfdeadbe235336eb259e33fc1ad55e761c23
Wenyu520521 <2584107537@qq.com>
2023-06-28 20:04:56 +0800

    首页主体-图片懒加载

---
+++ .history/src/components/library/index_20230628173653.js
@@ -0,0 +1,31 @@
+// 扩展vue原有的功能：全局组件，全局指令，自定义指令，挂载原型方法，注意：vue3没有过滤器
+// vue2插件写法：导出一个对象，对象中有一个install方法，install方法有两个参数，第一个参数是vue对象，第二个参数是可选的配置对象
+// vue3插件写法：导出一个对象，对象中有一个install方法，install方法有两个参数，第一个参数是app对象，第二个参数是可选的配置对象
+
+import XtxSkeleton from './xtx-skeleton.vue'
+import XtxCarousel from './xtx-carousel.vue'
+import XtxMore from './xtx-more.vue'
+import { define } from 'core-js/core/object'
+
+export default {
+  install (app, options) {
+    // 全局组件
+    // 在app上进行扩展，app提供component directive mount等方法
+    // 如果要挂载原型方法，可以使用app.config.globalProperties
+    app.component(XtxSkeleton.name, XtxSkeleton)
+    app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
+    // 定义指令
+    defineDirective(app)
+  }
+}
+
+// 定义指令
+const defineDirective = app => {
+  // 图片懒加载指令 v-lazy
+  // 原理：监听图片是否进入可视区，如果进入可视区，就将img的src属性设置为真实的图片地址
+  app.directive('lazy', {
+    // vue2.0 监听使用指令的dom是否创建好，钩子函数：inserted
+    // vue3.0 监听使用指令的dom是否创建好，钩子函数：mounted
+  })
+}
---
+++ .history/src/views/home/components/home-hot_20230628175900.vue
@@ -0,0 +1,60 @@
+<template>
+  <HomePanel ref="target" title="人气推荐" sub-title="人气爆款 不容错过">
+    <Transition name="fade">
+      <ul v-if="goods.length" class="goods-list">
+        <li v-for="item in goods" :key="item.id">
+          <RouterLink to="/">
+            <img ="item.picture" alt="" />
+            <p class="name">{{ item.title }}</p>
+            <p class="desc">{{ item.alt }}</p>
+          </RouterLink>
+        </li>
+      </ul>
+      <HomeSkeleton v-else />
+    </Transition>
+  </HomePanel>
+</template>
+
+<script>
+import HomePanel from './home-panel.vue'
+import HomeSkeleton from './home-skeleton.vue'
+import { findHot } from '@/api/home'
+import { useLazyData } from '@/hooks'
+export default {
+  name: 'HomeHot',
+  components: {
+    HomePanel,
+    HomeSkeleton
+  },
+  setup () {
+    // 获取人气推荐数据
+    const { target, result } = useLazyData(findHot)
+    return { goods: result, target }
+  }
+}
+</script>
+<style scoped lang="less">
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 426px;
+  li {
+    width: 306px;
+    height: 406px;
+    .hoverShadow();
+    img {
+      width: 306px;
+      height: 306px;
+    }
+    p {
+      font-size: 22px;
+      padding: 12px;
+      text-align: center;
+    }
+    .desc {
+      color: #999;
+      font-size: 18px;
+    }
+  }
+}
+</style>
